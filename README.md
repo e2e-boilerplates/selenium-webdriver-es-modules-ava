ec16dc39aa5fbfdfdb347cefbb79d36b96656d32
Girma Nigusse <xgirma@gmail.com>
2020-01-19 13:26:14 -0500

    set CI

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # selenium-webdriver (es6 modules, ava)
 
+[![GitHub Actions status | e2e-boilerplates/selenium-webdriver-es6modules-ava](https://github.com/e2e-boilerplates/selenium-webdriver-es6modules-ava/workflows/NodeCI/badge.svg)](https://github.com/e2e-boilerplates/selenium-webdriver-es6modules-ava/actions?workflow=NodeCI)
+
 selenium-webdriver end-to-end test automation boilerplate project. Using commonjs and ava.
 
 ## Getting Started
--- spec/search.spec.js
+++ spec/search.spec.js
@@ -1,12 +1,24 @@
 import test from "ava";
-import { Builder, By, Key, until } from "selenium-webdriver";
+import { Builder, By, Capabilities, Key, until } from "selenium-webdriver";
+import isCI from "is-ci";
+
+const chromeCapabilities = Capabilities.chrome();
 
 require("chromedriver");
 
 let browser;
 
+chromeCapabilities.set("goog:chromeOptions", {
+  args: ["--headless", "--no-sandbox", "--disable-gpu"]
+});
+
 test.before(async () => {
-  browser = await new Builder().forBrowser("chrome").build();
+  browser = isCI
+    ? await new Builder()
+        .forBrowser("chrome")
+        .withCapabilities(chromeCapabilities)
+        .build()
+    : await new Builder().forBrowser("chrome").build();
   browser.get("https://www.google.com");
 });
 
